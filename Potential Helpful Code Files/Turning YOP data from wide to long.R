f893e080c6408fb1b9ad0db174545b7c71bfee48
Rachel Chung <schung02@wesleyan.edu>
2020-10-12 10:06:54 -0700

    Adding notes to wide to long file
    Adding helpful notes for people to better understand wide to long csv file

--- Potential Helpful Code Files/Turning YOP data from wide to long.R
+++ Potential Helpful Code Files/Turning YOP data from wide to long.R
@@ -1,12 +1,23 @@
+#importing libraries
 library(janitor)
 library(reshape2)
+
+#importing csv data from GitHub repository on local desktop 
+yop <- read.csv("~/GitHub/Theophanes-Geography-Project/Datasets/Years_Place_Settlements GIS (CSV version).csv", header=FALSE)
+
+#when column names are being imported as the first row, use this to make the dataset's first row the column names 
 yop <- yop %>%row_to_names(row_number = 1)
 
-yop <- read.csv("~/GitHub/Theophanes-Geography-Project/Datasets/Years_Place_Settlements GIS (CSV version).csv", header=FALSE)
+#replace all NAs with 0s to make future calculations easier 
 yop[,21:550][is.na(yop[,21:550])] <- 0
+
+#create an long version with years over place 
 yop_long <- melt(yop, id.vars= colnames(yop[,1:20]))
-yop$`Total Instances(sum)` <- as.numeric(yop$`Total Instances(sum)`)
-       
-write.csv(yop_long, "yop_long.csv")   
-sum(as.numeric(as.character(yop_long$value)))       
-sum(as.numeric(yop$`Total Instances(sum)`))
+
+#below is an sanity check to check total instances in YOP wide against YOP long - their sums should be the same 
+sum(as.numeric(as.character(yop_long$value)),na.rm = T)       
+sum(as.numeric(yop$Total_Instances_Sum),na.rm = T)
+
+#exporting the long YOP dataset to an csv file for use outside of R
+write.csv(yop_long, "yop_long.csv")
+
